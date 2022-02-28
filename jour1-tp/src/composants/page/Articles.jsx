7ba4e436bbd9969055b99ec564ce110575d6b889
Malik <malik.h@webdevpro.net>
2022-02-28 14:53:59 +0100

    correction

--- jour1-tp/src/composants/page/Articles.jsx
+++ jour1-tp/src/composants/page/Articles.jsx
@@ -1,21 +1,46 @@
  
-import { Link , useLocation } from "react-router-dom"
+import { Link , useLocation , useNavigate } from "react-router-dom"
 import { useArticles } from "../../hook/useArticles"
+import {useState , useEffect} from "react";
+
 
 export function Articles () {
    
-    const articles = useArticles();
-    
+    const [articles , setArticles] = useState(useArticles());
+    const [ordre, setOrdre] = useState("ASC")
     const query = useLocation();
+    const navigate = useNavigate();
 
-    const q =  new URLSearchParams( query.search)
-    console.log(q.get("orderBy")) // "?orderBy=DESC" => DESC
-    
-    // [].reverse()
+
+    useEffect( () => {
+        const q = new URLSearchParams(query.search)
+        const orderBy = q.get("orderBy") ;
+        if(orderBy === "DESC"){
+            const cloneArticle = [...articles]; 
+            cloneArticle.reverse();
+            setArticles(cloneArticle)
+        } else {
+            setArticles(articles)
+        }
+    } , [ordre] )
+   
+    function manageOrder(){
+        if(ordre === "ASC"){
+            setOrdre("DESC")
+            navigate(`/articles?orderBy=DESC`)
+        }else {
+            setOrdre("ASC")
+            navigate(`/articles?orderBy=ASC`)
+        }
+    }
 
     return (
         <div className="row">
+            <header className="d-flex justify-content-between align-items-center py-3">
             <h1>Page des Articles</h1>
+            <button onClick={manageOrder} className="btn btn-dark">changer ordre</button>
+            </header>
+           
             { articles.map( (article, index) => {
                 return <div key={index} className="col-4">
                     <article  className="card">
