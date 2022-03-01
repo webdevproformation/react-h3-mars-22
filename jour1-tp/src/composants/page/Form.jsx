9d7beab44e3bfb13fcff03c535b11604f7aacd0a
Malik <malik.h@webdevpro.net>
2022-03-01 09:20:44 +0100

    correction

--- jour1-tp/src/composants/page/Form.jsx
+++ jour1-tp/src/composants/page/Form.jsx
@@ -1,21 +1,42 @@
 import { useState , useContext } from "react";
 import { UserContext } from "../../context/userContext";
+import { DataContext } from "../../context/dataContext";
 
 export const Form = () => {
-    const [form, setForm ] = useState({nom : ""})
+    const [form, setForm ] = useState({nom : "" , age : ""})
     const {setUser} = useContext(UserContext)
+    const {setData} = useContext(DataContext)
+   
+    function onChange(e){
+        const {name , value} = e.target
+        setForm({...form, [name] :value })
+    }
 
     function onSubmit(e){
         e.preventDefault();
         setUser( prevState => {
-            return {...prevState , nom : form.nom}
+            return {...prevState , nom : form.nom }
         } )
        // console.log(form);
     }
+    function onSubmit2(e){
+        e.preventDefault();
+        setData(prevState => {
+            return {...prevState , age : form.age}
+        })
+       // console.log(form);
+    }
+
     return <>
         <h1>Modification d'une valeur dans le context UserContext</h1>
         <form onSubmit={onSubmit}>
-            <input text="text" value={form.nom} onChange={e =>  setForm({...form, nom :e.target.value}) }/>
+            <input text="text" value={form.nom} onChange={ onChange } name="nom"/>
+            <input type="submit" />
+        </form>
+
+        <h2>deuxieme formulaire</h2>
+        <form onSubmit={onSubmit2}>
+            <input text="text" value={form.age} onChange={ onChange } name="age"/>
             <input type="submit" />
         </form>
     </>
--- jour1-tp/src/context/dataContext.jsx
+++ jour1-tp/src/context/dataContext.jsx
@@ -1,18 +1,18 @@
-import { createContext } from "react";
+import { createContext , useState} from "react";
 
 // 1 créer le context
 export const DataContext = createContext()
 
 // créer un composant qui retourne Provider 
 export function DataContextProvider( {children} ){
-
-    const data = {
+    const [data, setData] = useState({
         nom : "Zorro",
         age : 44 ,
         formation : ["js", "react" , "Angular"]
-    }
+    });
 
-    return <DataContext.Provider value={ data }>
+
+    return <DataContext.Provider value={ {...data , setData} }>
         {children}
     </DataContext.Provider>
 }
