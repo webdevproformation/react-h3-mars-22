42c54c3d6412f6bc27cccc32dff5330bf9304df3
Malik <malik.h@webdevpro.net>
2022-03-01 10:04:25 +0100

    compteur avec useState

--- jour2-tp/src/composants/Page1.jsx
+++ jour2-tp/src/composants/Page1.jsx
@@ -1,1 +1,13 @@
-export const Page1 = () => <>page 1</>
+import { useState } from "react"
+
+export const Page1 = () => {
+
+    const [nb, setNb] = useState(0)
+
+    return <>
+        <span>{nb}</span>
+        <button className="btn btn-danger" onClick={() => setNb(prev => prev - 1)}>-</button>
+        <button className="btn btn-success" onClick={() => setNb(prev => prev + 1)}>+</button>
+    </>
+
+}
