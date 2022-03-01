654e726c43d6fe30b7ee442a65e1d9cce412e527
Malik <malik.h@webdevpro.net>
2022-03-01 17:14:24 +0100

    questions

---
+++ question/00-question.js
@@ -0,0 +1,50 @@
+
+[
+    {
+       id : 1 ,
+       question : "faites vous du js ??" ,
+       reponse1 : {
+           text : "oui",
+           next : 2
+       } ,
+       reponse2 : {
+            text : "non",
+            next : 3
+        } 
+    },
+    {
+        id : 2 ,
+        question : "vous avez répondu oui à js? et du PHP ??" ,
+        reponse1 : {
+            text : "oui",
+            next : 4
+        } ,
+        reponse2 : {
+             text : "non",
+             next : 5
+         } 
+     },
+     {
+        id : 3 ,
+        question : "vous ne faîtes pas de js? et du HTML ??" ,
+        reponse1 : {
+            text : "oui",
+            next : 6
+        } ,
+        reponse2 : {
+             text : "non",
+             next : 7
+         } 
+     }
+]
+
+
+"1-oui-3-non" => ArrayList 
+
+md5() => fonction de hashage 
+// https://www.npmjs.com/package/md5
+
+[] =>   []  =>   []
+
+MongoDB => https://www.mongodb.com/atlas
+""
